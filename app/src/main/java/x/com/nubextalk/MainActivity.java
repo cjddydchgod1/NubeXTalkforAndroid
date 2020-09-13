deceb455cbc88e29c50c68ae465113e8cdc49340
JeongGod <jeonggu223@gmail.com>
2020-09-13 18:18:32 +0900

    [이정규] 2020.09.13 1. Profile객체를 이용하여 LinkedList생성 2. RecycleView를 이용한 친구목록 생성

--- app/src/main/java/x/com/nubextalk/MainActivity.java
+++ app/src/main/java/x/com/nubextalk/MainActivity.java
@@ -7,15 +7,21 @@ package x.com.nubextalk;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import x.com.nubextalk.item.Profile;
 
 /**
  * Github Commint Message는 다음을 따라주시길 바랍니다.
@@ -27,12 +33,25 @@ import androidx.core.app.ActivityCompat;
  * ....
  */
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private final LinkedList<Profile> mFriendList = new LinkedList<>();
+    private RecyclerView mRecycleView;
+    private FriendListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mFriendList.addLast(new Profile(0, 0, "친구 프로필"));
+        mFriendList.addLast(new Profile(R.drawable.cat1, R.drawable.baseline_fiber_manual_record_black_24dp, "친구1"));
+        mFriendList.addLast(new Profile(R.drawable.cat2, R.drawable.baseline_fiber_manual_record_black_24dp, "친구2"));
+        mFriendList.addLast(new Profile(R.drawable.cat3, R.drawable.baseline_fiber_manual_record_black_24dp, "친구3"));
+        mFriendList.addLast(new Profile(R.drawable.cat4, R.drawable.baseline_fiber_manual_record_black_24dp, "친구4"));
+        mFriendList.addLast(new Profile(R.drawable.cat5, R.drawable.baseline_fiber_manual_record_black_24dp, "친구5"));
+        mRecycleView = findViewById(R.id.friendRecycleview);
+        mAdapter = new FriendListAdapter(this, mFriendList);
+        mRecycleView.setAdapter(mAdapter);
+        mRecycleView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     private void initPermission() {

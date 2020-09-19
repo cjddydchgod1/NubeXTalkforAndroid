1fd775aa1a4e0ec5c71141efff88c740d666f374
Jae Young Choi <uniform3000@naver.com>
2020-09-20 01:45:21 +0900

    [최재영] 2020.09.20
    1. 채팅목록 설정 기능 구현
        - 채팅목록 알림설정 기능
        - 채팅목록 상단고정 기능
        - 채팅목록 나가기 기능
    2. 채팅목록 예정 기능
        - 채팅방 대화상대 추가 기능

--- app/src/main/java/x/com/nubextalk/ChatListActivity.java
+++ app/src/main/java/x/com/nubextalk/ChatListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,77 +17,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 
 public class ChatListActivity extends AppCompatActivity implements View.OnClickListener {
-
-    public class ChatList {
-        private int profileUrl;
-        private int statusUrl;
-        private String strProfileUrl;
-        private String strStatusUrl;
-        private String name;
-        private String msg;
-        private String time;
-        private String remain;
-
-        public ChatList(int url, String name, String msg, String time, String remain, int statusUrl) {
-            this.profileUrl = url;
-            this.name = name;
-            this.msg = msg;
-            this.time = time;
-            this.remain = remain;
-            this.statusUrl = statusUrl;
-        }
-
-        public ChatList(String strUrl, String name, String msg, String time, String remain, String strProfileUrl) {
-            this.strProfileUrl = strUrl;
-            this.name = name;
-            this.msg = msg;
-            this.time = time;
-            this.remain = remain;
-            this.strProfileUrl = strProfileUrl;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getMsg() {
-            return msg;
-        }
-
-        public String getTime() {
-            return time;
-        }
-
-        public String getRemain() {
-            return remain;
-        }
-
-
-        public int getProfileUrl() {
-            return profileUrl;
-        }
-
-        public String getStrStatusUrl() {
-            return strStatusUrl;
-        }
-
-        public String getStrProfileUrl() {
-            return strProfileUrl;
-        }
-
-        public int getStatusUrl() {
-            return statusUrl;
-        }
-    }
-
+    SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private final LinkedList<ChatList> mChatList = new LinkedList<>();
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private FloatingActionButton fab_main, fab_sub1, fab_sub2;
     private boolean isFabOpen = false;
+    private final int On = 1;
+    private final int Off = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,13 +38,26 @@ public class ChatListActivity extends AppCompatActivity implements View.OnClickL
         Toolbar toolbar = findViewById(R.id.chat_list_toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
-
-        mChatList.addLast(new ChatList(R.drawable.exmaple_profile, "최재영", "잘지내니???", "11:23", "1", R.drawable.oval_status_off));
-        mChatList.addLast(new ChatList(R.drawable.example_profile2, "장주섭", "전화 좀 받아봐...", "15:23", "2", R.drawable.oval_status_on));
-        mChatList.addLast(new ChatList(R.drawable.example_profile3, "이정규", "내가 미안하다고!", "04:21", "1", R.drawable.oval_status_off));
-        mChatList.addLast(new ChatList(R.drawable.exmaple_profile4, "박현준", "잠깐 나와봐. 할 말 있어", "16:53", "1", R.drawable.oval_status_off));
-
-
+        try {
+            mChatList.addLast(new ChatList(R.drawable.exmaple_profile, "최재영", "잘지내니???", dataFormat.parse("2020-09-13 13:24:22"), "1", On));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        try {
+            mChatList.addLast(new ChatList(R.drawable.example_profile2, "장주섭", "전화 좀 받아봐...", dataFormat.parse("2020-09-02 01:21:33"), "2", Off));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        try {
+            mChatList.addLast(new ChatList(R.drawable.example_profile3, "이정규", "내가 미안하다고!", dataFormat.parse("2020-09-15 14:20:10"), "1", Off));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        try {
+            mChatList.addLast(new ChatList(R.drawable.exmaple_profile4, "박현준", "잠깐 나와봐. 할 말 있어", dataFormat.parse("2020-09-17 12:30:45"), "1", On));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         mRecyclerView = findViewById(R.id.chat_list_view);
         mAdapter = new ChatListAdapter(this, mChatList);
@@ -154,26 +110,26 @@ public class ChatListActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.action_search:
-//                return true;
-//
-//            default:
-//                // If we got here, the user's action was not recognized.
-//                // Invoke the superclass to handle it.
-//                return super.onOptionsItemSelected(item);
-//
-//        }
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.chat_list_menu, menu);
 
         MenuItem searchItem = menu.findItem(R.id.action_search);
-//        SearchView searchView = (SearchView) searchItem.getActionView();
+        SearchView searchView = (SearchView) searchItem.getActionView();
 
         // Configure the search info and add any event listeners...
 

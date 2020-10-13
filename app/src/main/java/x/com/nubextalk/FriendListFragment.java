fcdf0d347137c89defd35e9c198957c83e3aa916
이정규 <jeonggu223@gmail.com>
2020-10-14 00:29:20 +0900

    [이정규] 2020.10.14
    1. 이름에서 '수정'버튼을 클릭하면 realm데이터 수정 구현.
    2. 완료버튼을 누를시, fragment 새로고침 구현

--- app/src/main/java/x/com/nubextalk/FriendListFragment.java
+++ app/src/main/java/x/com/nubextalk/FriendListFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -16,11 +17,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -86,7 +90,6 @@ public class FriendListFragment extends Fragment implements FriendListAdapter.on
 
     @Override
     public void onSelected(User address) {
-        Log.e("asdfasdf", "Clicked");
         initBottomsheet(address);
         new AnimManager(
                 AnimManager.make(mBottomWrapper, AnimManager.SHORT).translationY(3000).translationY(0).setInterpolator(new DecelerateInterpolator())
@@ -94,20 +97,56 @@ public class FriendListFragment extends Fragment implements FriendListAdapter.on
     }
 
     protected void initBottomsheet(User address) {
-        Log.e("clicked!!", "cliked!!!");
-
+        // 내 프로필과 친구 프로필에서 이미지 수정버튼 유무
         if(address.getUid().equals("1")){
-            Log.e("내꺼야", "Clicked!!");
             mBottomWrapper.findViewById(R.id.modifyImage).setVisibility(View.VISIBLE);
         } else {
             mBottomWrapper.findViewById(R.id.modifyImage).setVisibility(View.INVISIBLE);
         }
+        TextView profileName = mBottomWrapper.findViewById(R.id.profileName);
+        ImageView profileImage = mBottomWrapper.findViewById(R.id.profileImage);
+        // 이미지인데 Glide를 쓰면 안된다. String으로 바꿔야 하지 않을까?
+        ImageView profileStatus = mBottomWrapper.findViewById(R.id.profileStatus);
+        EditText modifyName = mBottomWrapper.findViewById(R.id.modifyName);
+        TextView modifyButton = mBottomWrapper.findViewById(R.id.modifyButton);
 
-        ((TextView) mBottomWrapper.findViewById(R.id.profileName)).setText(address.getName());
-        ImageView image = mBottomWrapper.findViewById(R.id.profileImage);
-        Glide.with(getContext()).load(address.getProfileImg()).into(image);
-        ImageView status = mBottomWrapper.findViewById(R.id.profileStatus);
+        profileName.setText(address.getName());
+        Glide.with(getContext()).load(address.getProfileImg()).into(profileImage);
 
+
+        // 수정 버튼
+        modifyButton.setOnClickListener(v -> {
+            String buttonName = modifyButton.getText().toString();
+            // 수정버튼을 눌렀을 경우
+            if(buttonName.equals("수정")) {
+                Log.e("수정", "Clicked");
+                modifyButton.setText("완료");
+                String temp = profileName.getText().toString();
+                profileName.setVisibility(View.GONE);
+                modifyName.setVisibility(View.VISIBLE);
+                modifyName.setText(temp);
+            } else {
+                Log.e("완료", "Clicked");
+                modifyButton.setText("수정");
+                String temp = modifyName.getText().toString();
+                modifyName.setVisibility(View.GONE);
+                profileName.setVisibility(View.VISIBLE);
+                profileName.setText(address.getName());
+
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        User user = realm.where(User.class).equalTo("uid", address.getUid()).findFirst();
+                        user.setName(temp);
+                    }
+                });
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.detach(this).attach(this).commit();
+            }
+        });
+
+
+        // 닫기 버튼
         mBottomWrapper.findViewById(R.id.mClose).setOnClickListener(v -> onBackPressed());
     }
 

82e592f2a7458bb3b6be20fdc95cd7efa8f3cd85
이정규 <jeonggu223@gmail.com>
2021-01-07 23:26:28 +0900

    [이정규] 2021.01.07
    1. myProfile RecyclerView에 띄우던 것을 Fragment에 띄우는 것으로 수정

--- app/src/main/java/x/com/nubextalk/Module/Adapter/FriendListAdapter.java
+++ app/src/main/java/x/com/nubextalk/Module/Adapter/FriendListAdapter.java
@@ -50,38 +50,22 @@ public class FriendListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View mItemView;
-        if(viewType == 0) {
-            mItemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_myprofile,parent,false);
-            return new MyProfileViewHolder(mItemView);
-        } else {
-            mItemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friend,parent,false);
-            return new FriendViewHolder(mItemView);
-        }
-
+        mItemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friend,parent,false);
+        return new FriendViewHolder(mItemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         // LinkedList에서 하나씩.
         User mCurrent = mDataSet.get(position);
-        // FriendList uid를 이용하여 비교한다.
-        if(mCurrent.getUid().equals(uid)) {
-            MyProfileViewHolder myProfileViewHolder = (MyProfileViewHolder) holder;
-            myProfileViewHolder.bintTo(mCurrent);
-            myProfileViewHolder.itemView.setOnClickListener(v -> {
-                if(listener != null) {
-                    listener.onSelected(mDataSet.get(position));
-                }
-            });
-        } else {
-            FriendViewHolder friendViewHolder = (FriendViewHolder) holder;
-            friendViewHolder.bintTo(mCurrent);
-            friendViewHolder.itemView.setOnClickListener(v -> {
-                if(listener != null) {
-                    listener.onSelected(mDataSet.get(position));
-                }
-            });
-        }
+
+        FriendViewHolder friendViewHolder = (FriendViewHolder) holder;
+        friendViewHolder.bintTo(mCurrent);
+        friendViewHolder.itemView.setOnClickListener(v -> {
+            if(listener != null) {
+                listener.onSelected(mDataSet.get(position));
+            }
+        });
     }
 
     @Override
@@ -89,15 +73,6 @@ public class FriendListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         return mDataSet.size();
     }
 
-    @Override
-    public int getItemViewType(int position) {
-        /**
-         * 첫 번째(myProfile)이면 0, 아니면 1
-         */
-        User user = mDataSet.get(position);
-        if(user.getUid().equals(uid)) return 0;
-        else return 1;
-    }
 
     public class FriendViewHolder extends RecyclerView.ViewHolder {
         private final TextView profileName;
@@ -110,44 +85,6 @@ public class FriendListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             profileName = itemView.findViewById(R.id.profileName);
             profileImage = itemView.findViewById(R.id.profileImage);
             profileStatus = itemView.findViewById(R.id.profileStatus);
-        }
-        public void bintTo(User user) {
-            String name = user.getDepartment() + " ";
-            if(user.getNickname()==null) {
-                name += user.getName();
-            } else {
-                name += user.getNickname();
-            }
-            profileName.setText(name);
-            if(!user.getProfileImg().isEmpty()){
-                aq.view(profileImage).image(user.getProfileImg());
-            }
-            // 초록
-            switch(user.getStatus()) {
-                case 0 :
-                    aq.view(profileStatus).image(R.drawable.baseline_fiber_manual_record_teal_a400_24dp);
-                    break;
-                case 1 :
-                    aq.view(profileStatus).image(R.drawable.baseline_fiber_manual_record_yellow_50_24dp);
-                    break;
-                case 2 :
-                    aq.view(profileStatus).image(R.drawable.baseline_fiber_manual_record_red_800_24dp);
-                    break;
-            }
-        }
-    }
-
-    public class MyProfileViewHolder extends RecyclerView.ViewHolder {
-        private final TextView profileName;
-        private final ImageView profileImage;
-        private final ImageView profileStatus;
-
-        public MyProfileViewHolder(@NonNull View itemView) {
-            super(itemView);
-            // item_friend.xml에서 불러온다.
-            profileName = itemView.findViewById(R.id.my_profileName);
-            profileImage = itemView.findViewById(R.id.my_profileImage);
-            profileStatus = itemView.findViewById(R.id.my_profileStatus);
         }
         public void bintTo(User user) {
             String name = user.getDepartment() + " ";

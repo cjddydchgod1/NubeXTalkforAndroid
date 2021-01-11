6ed6c2fd5fc0ad1483d852b557d8776bebed4147
chlwodud77 <uniform3000@naver.com>
2021-01-11 12:15:11 +0900

    [최재영] 2021.01.11
    1. 기존 activity_login.xml -> activity_login2.xml 로 수정

--- app/src/main/java/x/com/nubextalk/LoginActivity2.java
+++ app/src/main/java/x/com/nubextalk/LoginActivity2.java
@@ -45,7 +45,7 @@ public class LoginActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login2);
         realm           = Realm.getInstance(UtilityManager.getRealmConfig());
         final LoginActivity2 activity = this;
 

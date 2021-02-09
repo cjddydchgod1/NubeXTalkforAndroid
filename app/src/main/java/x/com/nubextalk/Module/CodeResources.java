c0818d4338a04c0f3d84297e9a9e925fe91ec37f
이정규 <jeonggu223@gmail.com>
2021-02-09 20:42:16 +0900

    [이정규] 2021.02.09
    1. Setting Fragment xml구현

--- app/src/main/java/x/com/nubextalk/Module/CodeResources.java
+++ app/src/main/java/x/com/nubextalk/Module/CodeResources.java
@@ -9,8 +9,13 @@ public class CodeResources {
     public static final String TAG = "NubeX Talk";
 
     /** SettingsActivity Resourece */
-    public static final int EXE_LOGOUT  = 0;
-    public static final int EXE_SWITCH  = 1;
+    public static final int EXE_SCREENLOCK  = 0;
+    public static final int EXE_ALARM       = 1;
+    public static final int EXE_SCREENMODE  = 2;
+    public static final int EXE_HOWTOUSE    = 3;
+    public static final int EXE_LOGOUT      = 4;
+    public static final int EXE_VERSIONINFO = 5;
+
     /** SharePACSActivity Resource */
     public static final int NON_RADIO   = 0;
     public static final int RADIO       = 1;
--- app/src/main/java/x/com/nubextalk/Module/Fragment/SettingFragment.java
+++ app/src/main/java/x/com/nubextalk/Module/Fragment/SettingFragment.java
@@ -26,68 +26,97 @@ import io.realm.Realm;
 import x.com.nubextalk.LoginActivity;
 import x.com.nubextalk.Manager.UtilityManager;
 import x.com.nubextalk.Model.Config;
-import x.com.nubextalk.Module.CodeResources;
 import x.com.nubextalk.R;
-import x.com.nubextalk.Module.CodeResources.*;
-
-import static x.com.nubextalk.Module.CodeResources.EXE_LOGOUT;
-import static x.com.nubextalk.Module.CodeResources.EXE_SWITCH;
+import static x.com.nubextalk.Module.CodeResources.*;
 
 public class SettingFragment extends Fragment implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
     private ViewGroup rootview;
     private Realm realm;
-    private LinearLayout mWrapperUser, mWrapperApp;
+    private LinearLayout mWrapperScreenLock, mWrapperSetAlarm, mWrapperSetTheme, mWrapperLogout, mWrapperHowToUse, mWrapperVesionInfo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        realm           = Realm.getInstance(UtilityManager.getRealmConfig());
-        rootview        = (ViewGroup) inflater.inflate(R.layout.fragment_setting, container, false);
-        mWrapperUser    = rootview.findViewById(R.id.wrapperUser);
-        mWrapperApp     = rootview.findViewById(R.id.wrapperApp);
+        realm               = Realm.getInstance(UtilityManager.getRealmConfig());
+        rootview            = (ViewGroup) inflater.inflate(R.layout.fragment_setting, container, false);
+
+        cleanView(mWrapperScreenLock    = rootview.findViewById(R.id.wrapperScreenLock));
+        cleanView(mWrapperSetAlarm      = rootview.findViewById(R.id.wrapperSetAlarm));
+        cleanView(mWrapperSetTheme      = rootview.findViewById(R.id.wrapperSetTheme));
+        cleanView(mWrapperHowToUse      = rootview.findViewById(R.id.wrapperHowToUse));
+        cleanView(mWrapperLogout        = rootview.findViewById(R.id.wrapperLogout));
+        cleanView(mWrapperVesionInfo    = rootview.findViewById(R.id.wrapperVesionInfo));
+
 
         initView();
         return rootview;
     }
 
-    private void cleanView() {
-        mWrapperUser.removeAllViews();
-        mWrapperUser.invalidate();
-
-        mWrapperApp.removeAllViews();
-        mWrapperApp.invalidate();
+    private void cleanView(LinearLayout linearLayout) {
+        linearLayout.removeAllViews();
+        linearLayout.invalidate();
     }
 
     private void initView() {
-        cleanView();
-
         LayoutInflater inflater = LayoutInflater.from(getContext());
         RelativeLayout l;
+
+        /** APP **/
+        l = (RelativeLayout) inflater.inflate(R.layout.item_settings_switch, null, false);
+        ((IconTextView) l.findViewById(R.id.titleRow)).setText("화면 잠금");
+        Switch lockSwitch = l.findViewById(R.id.switchRow);
+        lockSwitch.setChecked(false);
+        lockSwitch.setOnCheckedChangeListener(this);
+        lockSwitch.setTag(EXE_SCREENLOCK);
+        l.setOnClickListener(v -> lockSwitch.performClick());
+        mWrapperScreenLock.addView(l);
+
+        l = (RelativeLayout) inflater.inflate(R.layout.item_settings_switch, null, false);
+        ((IconTextView) l.findViewById(R.id.titleRow)).setText("알람 설정");
+        Switch AlarmSwitch = l.findViewById(R.id.switchRow);
+        AlarmSwitch.setChecked(false);
+        AlarmSwitch.setOnCheckedChangeListener(this);
+        AlarmSwitch.setTag(EXE_ALARM);
+        l.setOnClickListener(v -> AlarmSwitch.performClick());
+        mWrapperSetAlarm.addView(l);
+
+        /** dark mode **/
+        l = (RelativeLayout) inflater.inflate(R.layout.item_settings_simple, null, false);
+        ((IconTextView) l.findViewById(R.id.titleRow)).setText("테마설정");
+        l.setTag(EXE_SCREENMODE);
+        l.setOnClickListener(this);
+        mWrapperSetTheme.addView(l);
 
         /** USER **/
         l = (RelativeLayout) inflater.inflate(R.layout.item_settings_simple, null, false);
         ((IconTextView) l.findViewById(R.id.titleRow)).setText("로그아웃");
         l.setTag(EXE_LOGOUT);
         l.setOnClickListener(this);
-        mWrapperUser.addView(l);
+        mWrapperLogout.addView(l);
 
-        /** APP **/
-        l = (RelativeLayout) inflater.inflate(R.layout.item_settings_switch, null, false);
-        ((IconTextView) l.findViewById(R.id.titleRow)).setText("화면 잠금");
-        Switch mSwitch = l.findViewById(R.id.switchRow);
-        mSwitch.setChecked(false);
-        mSwitch.setOnCheckedChangeListener(this);
-        mSwitch.setTag(EXE_SWITCH);
-        l.setOnClickListener(v -> mSwitch.performClick());
-        mWrapperApp.addView(l);
+        /** how to use **/
+        l = (RelativeLayout) inflater.inflate(R.layout.item_settings_simple, null, false);
+        ((IconTextView) l.findViewById(R.id.titleRow)).setText("도움말");
+        l.setTag(EXE_HOWTOUSE);
+        l.setOnClickListener(this);
+        mWrapperHowToUse.addView(l);
+
+        /** Version info **/
+        l = (RelativeLayout) inflater.inflate(R.layout.item_settings_simple, null, false);
+        ((IconTextView) l.findViewById(R.id.titleRow)).setText("Vesion 1.0.0");
+        l.setTag(EXE_VERSIONINFO);
+        l.setOnClickListener(this);
+        mWrapperVesionInfo.addView(l);
     }
 
     @Override
     public void onClick(View view) {
         Context context = getContext();
         switch ((int) view.getTag()) {
+            case EXE_HOWTOUSE:
+                break;
             case EXE_LOGOUT:
                 realm.executeTransaction(realm1 -> {
                     Config config = Config.getMyAccount(realm1);
@@ -98,6 +127,8 @@ public class SettingFragment extends Fragment implements View.OnClickListener, C
                 startActivity(intent);
                 getActivity().finish();
                 break;
+            case EXE_VERSIONINFO:
+                break;
         }
 
     }
@@ -105,7 +136,9 @@ public class SettingFragment extends Fragment implements View.OnClickListener, C
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         switch ((int)buttonView.getTag()) {
-            case EXE_SWITCH:
+            case EXE_SCREENLOCK:
+                break;
+            case EXE_ALARM:
                 break;
         }
     }

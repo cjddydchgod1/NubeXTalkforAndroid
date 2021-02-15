2b021b710fc2c1e91f73f566f81c78a94bd50bd7
JuseobJang <wntjq68@gmail.com>
2021-02-15 16:55:58 +0900

    [장주섭] 2021.02.15
    1. merge시 발생한 에러 수정

--- app/src/main/java/x/com/nubextalk/Manager/FireBase/FirebaseMsgService.java
+++ app/src/main/java/x/com/nubextalk/Manager/FireBase/FirebaseMsgService.java
@@ -220,17 +220,7 @@ public class FirebaseMsgService extends FirebaseMessagingService {
                                         }
                                         // realm ChatRoom, ChatContent 생성
                                         payload.put("isFirst", true);
-                                        ChatRoom.createChatRoom(realm1, value, userIdList, new ChatRoom.onChatRoomCreatedListener() {
-                                            @Override
-                                            public void onCreate(ChatRoom chatRoom) {
-                                                if (!Config.getMyUID(realm1).equals(uid)) {
-                                                    ChatRoom roomInfo = realm1.where(ChatRoom.class).equalTo("rid", rid).findFirst();
-                                                    int channelId = Integer.parseInt(roomInfo.getNotificationId());
-                                                    makeChannel(CHANNEL_ID);
-                                                    notificationManager.notify(channelId, makeBuilder(rid, uid, type, content).build());
-                                                }
-                                            }
-                                        });
+                                        ChatRoom.createChatRoom(realm1, value, userIdList, null);
                                         ChatContent.createChat(realm1, payload);
 
                                         if (!Config.getMyUID(realm1).equals(uid)) {

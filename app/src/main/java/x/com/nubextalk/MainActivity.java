026c318d0e6a2fbafb2172238372cf22d2337fdb
JuseobJang <wntjq68@gmail.com>
2021-05-13 01:12:48 +0900

    [장주섭] 2021.05.13
    1. 로그인 버튼 수정
    2. initPermission() 토스트 메시지 주석 추가
    3. SettingFlagment 로그아웃 flag 설정 
    4. config.json

--- app/src/main/java/x/com/nubextalk/MainActivity.java
+++ app/src/main/java/x/com/nubextalk/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -169,10 +170,12 @@ public class MainActivity extends AppCompatActivity {
         PermissionListener pm = new PermissionListener() {
             @Override
             public void onPermissionGranted() {
+
             }
 
             @Override
             public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                //Toast.makeText(MainActivity.class, "", Toast.LENGTH_SHORT).show();
                 onBackPressed();
             }
         };
